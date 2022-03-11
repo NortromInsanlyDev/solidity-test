2f940f4e1be9cef9de103c3c412ec1d934313988
NortromInsanlyDev <codefan930@outlook.com>
2022-03-11 17:06:03 +0200

    update contract to pass tests

--- contracts/SolidityTest.sol
+++ contracts/SolidityTest.sol
@@ -1,27 +1,60 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract SolidityTest {
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "hardhat/console.sol";
+
+contract SolidityTest is Ownable, ERC721 {
     uint public constant PRICE = 1 ether / 10;
     uint public constant MAX_SUPPLY = 1000;
     uint public constant MAX_PER_TX = 5;
 
     // YOUR CODE HERE
     // ---
+    using Counters for Counters.Counter;
 
-    constructor() {
-        // YOUR CODE HERE
+    Counters.Counter private _tokenCounter;
+
+    modifier ableToMintFor(uint256 amount) {
+        require(MAX_SUPPLY >= _tokenCounter.current() + amount - 1, "Unable to mint requested amount");
+        _;
     }
 
-    function claim() external {
-        // YOUR CODE HERE
+    modifier onlyWhenEnoughFundToMint(uint256 amount) {
+        require(amount * PRICE == msg.value, "Insufficient fund to mint");
+        _;
     }
 
-    function mint(uint256 amount) external payable {
-        // YOUR CODE HERE
+    modifier withLimitPerTransaction(uint256 amount) {
+        require(amount <= MAX_PER_TX, "Too many amount to mint per transaction");
+        _;
     }
 
-    function ownerOf(uint256 tokenId) external view returns (address) {
+    constructor() ERC721("Test", "TST") {
         // YOUR CODE HERE
+        _tokenCounter.increment();
+    }
+
+    function claim() onlyOwner external {
+        // YOUR CODE HERE
+        (bool success, ) = msg.sender.call{value: address(this).balance}("");
+        require(success);
+    }
+
+    function mint(uint256 amount) ableToMintFor(amount) onlyWhenEnoughFundToMint(amount) withLimitPerTransaction(amount) external payable {
+        // YOUR CODE HERE
+        
+        for (uint256 index = 0; index < amount; index++) {
+            _safeMint(msg.sender, _tokenCounter.current());
+            _tokenCounter.increment();
+        }
+    }
+
+    function ownerOf(uint256 tokenId) override public view returns (address) {
+        // YOUR CODE HERE
+        return super.ownerOf(tokenId);
     }
 }
